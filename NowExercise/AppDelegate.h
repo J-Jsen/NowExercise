b2f414989353ca8a771515681b4234a7b5ce716d
ShengHao <641776671@qq.com>
2016-10-21 14:54:43 +0800

    Initial Commit

---
+++ NowExercise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NowExercise
+//
+//  Created by mac on 16/10/21.
+//  Copyright © 2016年 Guodong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
